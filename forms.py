2f241977365ce5513aa39276630ee6c2a879cc2f
Nimitsonani <imitsonani999@gmail.com>
2026-02-13 16:27:22 +0530

    Remove TODO comments and update form class headers

--- forms.py
+++ forms.py
@@ -13,20 +13,20 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
+#Create a RegisterForm to register new users
 class RegisterForm(FlaskForm):
     name = StringField('Username',validators=[DataRequired()])
     email = StringField('email',validators=[DataRequired()])
     password = StringField('Password',validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-# TODO: Create a LoginForm to login existing users
+#  Create a LoginForm to login existing users
 class LoginForm(FlaskForm):
     email = StringField('email',validators=[DataRequired()])
     password = StringField('password',validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# TODO: Create a CommentForm so users can leave comments below posts
+#  Create a CommentForm so users can leave comments below posts
 class CommentForm(FlaskForm):
     comment = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField('Submit Comment')
